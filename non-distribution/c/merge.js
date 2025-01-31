483dc7bbbc86c816ec7b6db8dfa0f953f75256c1
jcmigaly <joncarlo_migaly@brown.edu>
2025-01-31 03:53:36 +0000

    just need end-to-end and 8 tests

--- non-distribution/c/merge.js
+++ non-distribution/c/merge.js
@@ -50,20 +50,17 @@ const rl = readline.createInterface({
 // 1. Read the incoming local index data from standard input (stdin) line by line.
 let localIndex = '';
 rl.on('line', (line) => {
-  console.log(line);
   localIndex += line + '\n';
-  console.log(`HERE: ${localIndex}`);
 });
 
 rl.on('close', () => {
   // 2. Read the global index name/location, using process.argv
   // and call printMerged as a callback
-
-  fs.readFileSync(process.argv[2], 'utf-8', printMerged);
+  const globalIndex = process.argv[2];
+  fs.readFile(globalIndex, 'utf-8', printMerged);
 });
 
 const printMerged = (err, data) => {
-  // console.log("HERE")
   if (err) {
     console.error('Error reading file:', err);
     return;
@@ -87,7 +84,7 @@ const printMerged = (err, data) => {
     const freq = parseInt(parts[1], 10);
     const url = parts[2];
 
-    local[term] = {'url': url, 'freq': freq};
+    local[term] = {url, freq};
   }
 
   // 4. For each line in `globalIndexLines`, parse them and add them to the `global` object where keys are terms and values are arrays of `url` and `freq` objects.
@@ -95,59 +92,46 @@ const printMerged = (err, data) => {
   for (const line of globalIndexLines) {
     const parts = line.split('|').map((part) => part.trim()); // use map because we already have array going
     const term = parts[0];
-    const url_freq_string = parts[1];
-    // console.log(`url_freq_string: ${url_freq_string}`)
-    const freq_urls = url_freq_string.split(' ');
+    const urlFreqString = parts[1];
+    const freqUrls = urlFreqString.split(' ');
 
-    const url_freq_array = [];
+    const urlFreqArray = [];
 
-    for (let i = 0; i < freq_urls.length; i += 2) {
-      const url = freq_urls[i];
-      const freq = parseInt(freq_urls[i + 1], 10);
+    for (let i = 0; i < freqUrls.length; i += 2) {
+      const url = freqUrls[i];
+      const freq = parseInt(freqUrls[i + 1], 10);
 
-      if (isNaN(freq)) {
-        console.error(`Invalid frequency value for term ${term}: ${freq_urls[i + 1]}`);
-        continue; // Skip this pair if the frequency is invalid
-      }
-
-      if (!url) {
-        console.error(`Invalid URL value for term ${term}`);
-        continue; // Skip this pair if the URL is invalid
-      }
-
-      url_freq_array.push({'url': url, 'freq': freq});
+      urlFreqArray.push({'url': url, 'freq': freq});
     }
 
-    global[term] = url_freq_array; // Array of {url, freq} objects
+    global[term] = urlFreqArray; // Array of {url, freq} objects
   }
 
   // 5. Merge the local index into the global index:
   // - For each term in the local index, if the term exists in the global index:
   for (const key in local) {
-    const local_index_url = local[key].url;
-    const local_index_freq = local[key].freq;
+    const localIndexUrl = local[key].url;
+    const localIndexFreq = local[key].freq;
     if (key in global) {
       // - Append the local index entry to the array of entries in the global index.
       // - Sort the array by `freq` in descending order.
-      global[key].push({'url': local_index_url, 'freq': local_index_freq});
+      global[key].push({'url': localIndexUrl, 'freq': localIndexFreq});
       global[key].sort(compare);
-    }
-    // - If the term does not exist in the global index:
-    else {
+    } else {
       // - Add it as a new entry with the local index's data.
-      const url_freq_array = [];
-      url_freq_array.push({'url': local_index_url, 'freq': local_index_freq});
-      global[key] = url_freq_array;
+      const urlFreqArray = [];
+      urlFreqArray.push({'url': localIndexUrl, 'freq': localIndexFreq});
+      global[key] = urlFreqArray;
     }
   }
   // 6. Print the merged index to the console in the same format as the global index file:
   //    - Each line contains a term, followed by a pipe (`|`), followed by space-separated pairs of `url` and `freq`.
   for (const key in global) {
-    let master_string = '';
-    master_string += key + ' |';
+    let masterString = '';
+    masterString += key + ' |';
     global[key].forEach((item) => {
-      master_string += ` ${item.url} ${item.freq}`;
+      masterString += ` ${item.url} ${item.freq}`;
     });
-    console.log(master_string);
+    console.log(masterString);
   }
 };
--- non-distribution/c/process.sh
+++ non-distribution/c/process.sh
@@ -6,5 +6,5 @@
 # Convert each line to one word per line, **remove non-letter characters**, make lowercase, convert to ASCII; then remove stopwords (inside d/stopwords.txt)
 # Commands that will be useful: tr, iconv, grep
 
-tr ' ' '\n' | tr -cd 'a-zA-Z\n' | tr 'A-Z' 'a-z' | iconv -f UTF-8 -t ASCII//TRANSLIT | sed 's/[0-9]*//g' | sed 's/indexhtml//g' | sed 's/^level.*$/level/g' | grep -v -w -f ./d/stopwords.txt
+tr ' ' '\n' | tr -cd 'a-zA-Z\n' | tr '[:upper:]' '[:lower:]' | iconv -f UTF-8 -t ASCII//TRANSLIT | sed 's/[0-9]*//g' | sed 's/indexhtml//g' | sed 's/^level.*$/level/g' | grep -v -w -f ./d/stopwords.txt
 
