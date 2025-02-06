e10a9b367d8549c84a60640714c2324bea2d2951
jcmigaly <joncarlo_migaly@brown.edu>
2025-02-06 18:05:22 +0000

    bouta change functions

--- distribution/util/serialization.js
+++ distribution/util/serialization.js
@@ -1,179 +1,176 @@
-const { string } = require("yargs")
-
 function isBaseType(object) {
-  if (typeof object === "string" ||
-      typeof object === "number" ||
-      typeof object === "boolean" ||
+  if (typeof object === 'string' ||
+      typeof object === 'number' ||
+      typeof object === 'boolean' ||
       object === null ||
-      typeof object === "undefined") {
-        return true
-      }
-  return false
+      typeof object === 'undefined') {
+    return true;
+  }
+  return false;
 }
 
 function serializeBaseType(object) {
-  let transformedObject = {}
-  if (typeof object === "undefined") {
-    transformedObject["__meta__specialType__"] = { type: typeof object, value: ""}
-    return transformedObject
+  const transformedObject = {};
+  if (typeof object === 'undefined') {
+    transformedObject['__meta__specialType__'] = {type: typeof object, value: ''};
+    return transformedObject;
   } else if (object === null) {
-    transformedObject["__meta__specialType__"] = { type: "null", value: ""}
-    return transformedObject
+    transformedObject['__meta__specialType__'] = {type: 'null', value: ''};
+    return transformedObject;
   } else {
-    return object
+    return object;
   }
 }
 
 function serializeFunction(object) {
-  let stringFunc = object.toString()
+  const stringFunc = object.toString();
 
   // Get the part between the first '(' and ')'
-  const paramsStart = stringFunc.indexOf('(') + 1
-  const paramsEnd = stringFunc.indexOf(')')
-  const paramsString = stringFunc.slice(paramsStart, paramsEnd).trim()
+  const paramsStart = stringFunc.indexOf('(') + 1;
+  const paramsEnd = stringFunc.indexOf(')');
+  const paramsString = stringFunc.slice(paramsStart, paramsEnd).trim();
 
   // Split the parameters by commas and remove extra whitespace
-  const params = paramsString ? paramsString.split(',').map(param => param.trim()) : []
+  const params = paramsString ? paramsString.split(',').map((param) => param.trim()) : [];
 
   // Find the first occurence of { and }
-  const bodyStart = stringFunc.indexOf('{') + 1
-  const bodyEnd = stringFunc.lastIndexOf('}')
-  const bodyString = stringFunc.slice(bodyStart, bodyEnd).trim()
+  const bodyStart = stringFunc.indexOf('{') + 1;
+  const bodyEnd = stringFunc.lastIndexOf('}');
+  const bodyString = stringFunc.slice(bodyStart, bodyEnd).trim();
 
-  let funcObj = { type: 'function', parameters: params, body: bodyString }
-  let transformedObject = {}
-  transformedObject["__meta__specialType__"] = funcObj
-  return transformedObject
+  const funcObj = {type: 'function', parameters: params, body: bodyString};
+  const transformedObject = {};
+  transformedObject['__meta__specialType__'] = funcObj;
+  return transformedObject;
 }
 
 function serializeDate(object) {
   return {
-    __meta__specialType__: "Date",
-    value: object.toISOString() // Store the ISO string representation of the Date
-  }
+    __meta__specialType__: 'Date',
+    value: object.toISOString(), // Store the ISO string representation of the Date
+  };
 }
 
 function serializeError(error) {
   return {
     __meta__specialType__: 'Error',
     message: error.message,
-    stack: error.stack
-  }
+    stack: error.stack,
+  };
 }
 
 function serializeHelper(object) {
   // Base case in which we return a serialize
   if (isBaseType(object) === true) {
     // Serialize base-type
-    return serializeBaseType(object)
+    return serializeBaseType(object);
   }
 
   // Another base case in which we return a serialized function
   if (typeof object === 'function') {
-    return serializeFunction(object)
+    return serializeFunction(object);
   }
 
   // Base case for date objects
   if (object instanceof Date) {
-    return serializeDate(object)
+    return serializeDate(object);
   }
 
   // Base case for date objects
   if (object instanceof Error) {
-    return serializeError(object)
+    return serializeError(object);
   }
 
   // If object is array, iterate over each element
   if (Array.isArray(object)) {
-    return object.map(serializeHelper)
+    return object.map(serializeHelper);
   }
-  
-  // Handle objects
-  if (typeof object === "object") {
-    let serializedObj = {}
-    for (let key in object) {
-      serializedObj[key] = serializeHelper(object[key]) // Recursively serialize each property
-    }
-    return serializedObj
-  }
-  return 'Invalid Object Type'
 
+  // Handle objects
+  if (typeof object === 'object') {
+    const serializedObj = {};
+    for (const key in object) {
+      serializedObj[key] = serializeHelper(object[key]); // Recursively serialize each property
+    }
+    return serializedObj;
+  }
+  return 'Invalid Object Type';
 }
 
 function serialize(object) {
-  let transformedObject = serializeHelper(object)
-  return JSON.stringify(transformedObject)
+  const transformedObject = serializeHelper(object);
+  return JSON.stringify(transformedObject);
 }
 
 function deserializeBaseCase(object) {
   // Case 1: Typical base-case
-  if (typeof object === "string" || typeof object === "number" || typeof object === "boolean") {
-    return object
+  if (typeof object === 'string' || typeof object === 'number' || typeof object === 'boolean') {
+    return object;
   }
   // Case 2: null
   if (object.__meta__specialType__.type === 'null') {
-    return null
+    return null;
   }
   // Case 3: undefined
   if (object.__meta__specialType__.type === 'undefined') {
-    return undefined
+    return undefined;
   }
   // Case 4: Function
   if (object.__meta__specialType__.type === 'function') {
-    let func = new Function(...object.__meta__specialType__.parameters, object.__meta__specialType__.body)
-    return func
+    const func = new Function(...object.__meta__specialType__.parameters, object.__meta__specialType__.body);
+    return func;
   }
 }
 
 function deserializeDate(object) {
-  return new Date(object.value)
+  return new Date(object.value);
 }
 
 function deserializeError(object) {
-  const error = new Error(object.message)
-  error.stack = object.stack
-  return error
+  const error = new Error(object.message);
+  error.stack = object.stack;
+  return error;
 }
 
 function deserializeHelper(object) {
-  // Handle Date 
+  // Handle Date
   if (object.__meta__specialType__ === 'Date') {
-    return deserializeDate(object)
+    return deserializeDate(object);
   }
   // Handle Error
   if (object.__meta__specialType__ === 'Error') {
-    return deserializeError(object)
+    return deserializeError(object);
   }
   // Handle base case
   if (isBaseType(object) == true || object.__meta__specialType__) {
-    return deserializeBaseCase(object)
+    return deserializeBaseCase(object);
   }
   // Handle Arrays
   if (Array.isArray(object)) {
-    return object.map(deserializeHelper)
+    return object.map(deserializeHelper);
   }
-  // Handle Objects 
+  // Handle Objects
   if (typeof object === 'object') {
-    let deserializedObj = {}
-    for (let key in object) {
-      deserializedObj[key] = deserializeHelper(object[key])
+    const deserializedObj = {};
+    for (const key in object) {
+      deserializedObj[key] = deserializeHelper(object[key]);
     }
-    return deserializedObj
+    return deserializedObj;
   }
-  return object
+  return object;
 }
 
 function deserialize(string) {
-  let object = 'bad request'
+  let object = 'bad request';
   try {
     object = JSON.parse(string);
   } catch (error) {
   } finally {
-    let deserializedObj = deserializeHelper(object)
+    const deserializedObj = deserializeHelper(object);
     if (object === 'bad request') {
-      throw new SyntaxError("Invalid input")
+      throw new SyntaxError('Invalid input');
     }
-    return deserializedObj
+    return deserializedObj;
   }
 }
 
